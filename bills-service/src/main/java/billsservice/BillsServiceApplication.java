e2479dfec5eea10c75f84d0e03325020b741669d
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-14 11:04:02 +0300

    init project

---
+++ bills-service/src/main/java/billsservice/BillsServiceApplication.java
@@ -0,0 +1,13 @@
+package billsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsServiceApplication.class, args);
+	}
+
+}
---
+++ bills-service/src/test/java/billsservice/BillsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package billsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
