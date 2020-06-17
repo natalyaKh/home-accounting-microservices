4596551f631b3a4f62b4ded1fe06005abbf7dd91
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-17 11:09:17 +0300

    adding eureka-client to bill-service

--- bills-service/src/main/java/billsservice/GLOBALTODO.java
+++ bills-service/src/main/java/billsservice/GLOBALTODO.java
@@ -3,4 +3,5 @@ package billsservice;
 public class GLOBALTODO {
 //    TODO при создании счета дата создания null
 //    TODO check if user exist in all create (from tolen???) разрешить только себе
+//    TODO currency-service  made PingController
 }
