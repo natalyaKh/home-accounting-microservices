62e5af6d6b04649b76f4e997db321042262c6175
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-17 16:18:08 +0300

    adding logic currency-service=zuul with token

--- currency-service/src/main/java/currencyservice/CurrencyServiceApplication.java
+++ currency-service/src/main/java/currencyservice/CurrencyServiceApplication.java
@@ -3,6 +3,8 @@ package currencyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +14,9 @@ public class CurrencyServiceApplication {
 		SpringApplication.run(CurrencyServiceApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
 }
