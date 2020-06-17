475880d59e8bf00d8b7dfee54381a177771997fb
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-17 11:02:03 +0300

    adding eureka-client to users-service

--- users-service/src/main/java/usersservice/UsersServiceApplication.java
+++ users-service/src/main/java/usersservice/UsersServiceApplication.java
@@ -4,12 +4,14 @@ import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import usersservice.security.AppProperties;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UsersServiceApplication {
 
 	private static Environment env;
