5f6b3fff7d16099d457d5d3c059decbfd46bf174
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-17 11:06:10 +0300

    adding eureka-client to currency-service

--- currency-service/src/main/java/currencyservice/CurrencyServiceApplication.java
+++ currency-service/src/main/java/currencyservice/CurrencyServiceApplication.java
@@ -2,8 +2,10 @@ package currencyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CurrencyServiceApplication {
 
 	public static void main(String[] args) {
