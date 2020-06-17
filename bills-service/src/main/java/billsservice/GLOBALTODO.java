ba102b5c527f6c95bcaa7645f695430abe03ff87
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-17 18:19:19 +0300

    adding config-service (profile-native)

--- bills-service/src/main/java/billsservice/GLOBALTODO.java
+++ bills-service/src/main/java/billsservice/GLOBALTODO.java
@@ -4,4 +4,5 @@ public class GLOBALTODO {
 //    TODO при создании счета дата создания null
 //    TODO check if user exist in all create (from tolen???) разрешить только себе
 //    TODO currency-service  made PingController
+//    TODO разобраться с конфиг сервек + эврика
 }
