a99da6ce6f5b23b5408e3ee40d92d8bcc7e15561
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-22 11:50:52 +0300

    add ping controller to currency-service

---
+++ currency-service/src/main/java/currencyservice/controller/PingController.java
@@ -0,0 +1,16 @@
+package currencyservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @Autowired
+    Environment env;
+    @GetMapping("/test")
+    public String testWork() {
+        return "I am working on port: " + env.getProperty("server.port");
+    }
+}
